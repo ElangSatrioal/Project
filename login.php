31f5344f1c9fba80ec32ea7dccb6e48c696ae592
ElangSatrioal <elangsatrioalalyyu@gmail.com>
2023-03-24 14:11:24 +0700

    up

--- login.php
+++ login.php
@@ -7,13 +7,26 @@
 <link rel="stylesheet" type="text/css" href = "css/style.css">
     </head>  
 <body id = "bg-login">
-    <div class="box-login">
+    <div class = "box-login">
         <h2>Login</h2>
         <form action = "" method = "POST">
-            <input type="text" name="username" , placeholder = "Username" class="input-control">
+            <input type="text" name="user" , placeholder = "Username" class="input-control">
             <input type="password" name="pass" , placeholder = "Password" class="input-control">
             <input type="submit" name="submit" , value = "Login" class="btn">
         </form>
+        <?php
+          if(isset($_POST['submit'])){
+            include 'db.php';
+
+            $user = $_POST['user'];
+            $pass = $_POST['pass'];
+
+            $cek = mysqli_query($koneksi,"SELECT * FROM admin WHERE username = ' " .$user. "'  AND password = ' " .MD5($pass). "'" );
+            echo mysqli_num_rows($cek);
+          }
+          
+          
+          ?>
     </div>
 </body>
 </html>
